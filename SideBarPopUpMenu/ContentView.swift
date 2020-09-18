74b04a9ada410f28c30bfe8c678707e5bc9ebd91
George Davis IV <gdavisiv@gmail.com>
2020-09-17 21:54:36 -0400

    UIUpdate: Implementing Side Menu

--- SideBarPopUpMenu/ContentView.swift
+++ SideBarPopUpMenu/ContentView.swift
@@ -25,39 +25,62 @@ struct Home : View {
     @State var width = UIScreen.main.bounds.width
     
     var body: some View{
-        VStack{
-            ZStack {
-                HStack{
-                    Button(action: {}, label: {
-                        Image(systemName: "square.stack.3d.up")
-                            .font(.system(size: 22))
-                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                    })
-                    Spacer(minLength: 0)
-                    
-                    Button(action: {}, label: {
-                        Image("pic1")
-                            .resizable()
-                            .renderingMode(.original)
-                            .frame(width: 35, height: 35)
-                            .clipShape(Circle())
-                    })
-                }
-                
-                Text("Home")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-            }
-            .padding()
-            //.padding(.top,edges!.top)
-            .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
-            .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.1), radius: 5, x: 0, y: 5)
-            
-            Spacer(minLength: 0)
-            
+        ZStack {
+            //Top Menu
             VStack{
+                ZStack {
+                    HStack{
+                        Button(action: {}, label: {
+                            Image(systemName: "square.stack.3d.up")
+                                .font(.system(size: 22))
+                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                        })
+                        Spacer(minLength: 0)
+                        
+                        Button(action: {}, label: {
+                            Image("pic1")
+                                .resizable()
+                                .renderingMode(.original)
+                                .frame(width: 35, height: 35)
+                                .clipShape(Circle())
+                        })
+                    }
+                    
+                    Text("Home")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                }
+                .padding()
+                //.padding(.top,edges!.top)
+                .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.1), radius: 5, x: 0, y: 5)
                 
+                Spacer(minLength: 0)
+                
+                VStack{
+                    
+                }
             }
+            
+            //Side Menu
+            HStack(spacing: 0){
+                Spacer(minLength: 0)
+                VStack {
+                    HStack{
+                        Spacer(minLength: 0)
+                        
+                        Button(action: {}, label: {
+                            Image(systemName: "xmark")
+                                .font(.system(size: 22, weight: .bold))
+                                .foregroundColor(.white)
+                        })
+                    }
+                    Spacer(minLength: 0)
+                }
+                .frame(width: width - 100)
+                .background((Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))))
+            }
+            .background(Color.black.opacity(0.3))
         }
         //.ignoresSafeArea(.all, edges: .all)
     }
